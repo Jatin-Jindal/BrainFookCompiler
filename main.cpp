e4f46655ed0774114257c446bfc43d5e4af954fe
GhostMander <58628835+GhostMander@users.noreply.github.com>
2022-02-26 13:04:25 +0530

    refactor: moved large functions to the bottom of the file, and changed a few variable names.

--- main.cpp
+++ main.cpp
@@ -9,39 +9,14 @@ bool isBrainFuckCharacter(char letter);
 
 string getBrainFuckCode(ifstream &in);
 
+bool isValidCall(int argc, char const *argv[]);
+
 bool isAscii(int x) {
     return x >= 0 && x <= 127;
 }
 
-bool isFileEmpty(ifstream& pFile) {
-    return pFile.peek() == ifstream::traits_type::eof();
-}
-
-bool isValidCall(int argc, char const *argv[]) {
-
-    // Check if there is a file
-    if (argc != 2)
-        cout << "Usage: " << argv[0] << " <input_file>" << endl;
-
-    // Check if the file is a brainfuck file
-    else if (!regex_match(argv[1], regex(R"(^(.*[\\\/]|)[a-z_][a-z_0-9 ]*\.bf$)", iCase))) {
-        cout << "Invalid file name" << endl;
-        cout << "Enter a valid Brainfuck file name (*.bf)" << endl;
-    }
-
-    // Check if the file exists
-    else if (access(argv[1], F_OK) == -1)
-        cout << "File does not exist" << endl;
-
-    // Check if the file is readable
-    else if (access(argv[1], R_OK) == -1)
-        cout << "File is not readable" << endl;
-
-    // If any of the above conditions are not met, return 0
-    else {
-        cout << "FILE FOUND!" << endl;
-        return true;
-    } return false;
+bool isFileEmpty(ifstream &in) {
+    return in.peek() == ifstream::traits_type::eof();
 }
 
 int main(int argc, char const *argv[]) {
@@ -83,5 +58,34 @@ string getBrainFuckCode(ifstream &in) {
 
 bool isBrainFuckCharacter(char letter) {
     // Returns true if the character is a valid Brainfuck character (i.e. +-<>.,[]).
-    return letter == '>' || letter == '<' || letter == '+' || letter == '-' || letter == '.' || letter == ',' || letter == '[' || letter == ']';
+    return letter == '>' || letter == '<' || letter == '+' || letter == '-' || letter == '.' || letter == ',' ||
+           letter == '[' || letter == ']';
+}
+
+bool isValidCall(int argc, char const *argv[]) {
+
+    // Check if there is a file
+    if (argc != 2)
+        cout << "Usage: " << argv[0] << " <input_file>" << endl;
+
+        // Check if the file is a brainfuck file
+    else if (!regex_match(argv[1], regex(R"(^(.*[\\\/]|)[a-z_][a-z_0-9 ]*\.bf$)", iCase))) {
+        cout << "Invalid file name" << endl;
+        cout << "Enter a valid Brainfuck file name (*.bf)" << endl;
+    }
+
+        // Check if the file exists
+    else if (access(argv[1], F_OK) == -1)
+        cout << "File does not exist" << endl;
+
+        // Check if the file is readable
+    else if (access(argv[1], R_OK) == -1)
+        cout << "File is not readable" << endl;
+
+        // If any of the above conditions are not met, return 0
+    else {
+        cout << "FILE FOUND!" << endl;
+        return true;
+    }
+    return false;
 }
