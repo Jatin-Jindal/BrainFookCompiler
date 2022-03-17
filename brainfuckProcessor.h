85e381992f0322e438efbc88a97042a6e592b637
GhostMander <58628835+GhostMander@users.noreply.github.com>
2022-03-17 23:29:07 +0530

    bug: Broke loops and changed the iterations

--- brainfuckProcessor.h
+++ brainfuckProcessor.h
@@ -23,32 +23,47 @@ class BrainfuckProcessor {
         for (auto c: code) {
             if (c == '[') s++;
             if (c == ']') s--;
-            if (s < 0 || s > 1) return false;
+            if (s < 0) return false;
         }
         return s == 0;
     }
 
-    void computeLoop(std::queue<char> loop) {
-        std::string l;
-        while (!loop.empty()) { l += loop.front(); loop.pop(); }
-        u_char ch;
-        int loopAdd = p;
-        while (memory[loopAdd] != 0) {
-            for (auto c: l) {
-                if (c == '>') p = (p == 30000) ? 30000 : p+1;
-                else if (c == '<') p = (p == 00000) ? 00000 : p-1;
-                else if (c == '+') memory[p]++;
-                else if (c == '-') memory[p]--;
-                else if (c == '.') putchar(memory[p]);
-                else if (c == ',') { std::cin >> ch; memory[p] = int(ch); }
+    void computeLoop(std::string loop) {
+        int P = p;
+        while (memory[P] != 0) {
+            int i = 0;
+            while (i < loop.length()) {
+                char c = loop[i];
+                if (c == '+') memory[p]++;
+                if (c == '-') memory[p]--;
+                if (c == '>') p++;
+                if (c == '<') p--;
+                if (c == '.') std::cout << (char) memory[p];
+                if (c == ',') {
+                    u_char s;
+                    std::cin >> s;
+                    memory[p] = s;
+                }
+                if (c == '[') {
+                    int n = 1, j = i;
+                    while (n != 0) {
+                        j++;
+                        if (loop[j] == '[') n++;
+                        if (loop[j] == ']') n--;
+                    }
+                    std::string loop2 = code.substr(i + 1, j - i - 1);
+                    i = j;
+                    computeLoop(loop2);
+                }
             }
+            i++;
         }
     }
 
 public:
     BrainfuckProcessor() = default;
 
-    BrainfuckProcessor(std::string input) {
+    explicit BrainfuckProcessor(std::string input) {
         code = std::move(input);
         if (!valid()) throw std::runtime_error("Invalid code");
     }
@@ -62,25 +77,62 @@ public:
 
 
 void BrainfuckProcessor::parse() {
-    std::queue<char> loop;
-    int loopPtr;
-    int inLoop = 0; // 0 = outside loop, 1 = inside loop, 2 = skip loop
-    u_char ch;
-    for (auto c: code) {
-        if (inLoop == 2);
-        else if (inLoop == 1) { loop.push(c); }
-        else if (c == '>') p = (p == 30000) ? 30000 : p+1;
-        else if (c == '<') p = (p == 00000) ? 00000 : p-1;
-        else if (c == '+') memory[p]++;
-        else if (c == '-') memory[p]--;
-        else if (c == '.') putchar(memory[p]);
-        else if (c == ',') { std::cin >> ch; memory[p] = int(ch); }
-        else if (c == '[') {
-            if (memory[p] != 0) {
-                inLoop = 1;
-                loopPtr = p;
-            } else { inLoop = 2; }
+////    std::queue<char> loop;
+//    std::string loop;
+//    int inLoop = 0; // 0 = outside loop, 1 = inside loop, 2 = skip loop
+//    u_char ch;
+//    int n = 0;
+//    for (auto c: code) {
+//        if (inLoop == 2);
+//        else if (inLoop == 1 && c != ']') {
+//            n += (c == '[');
+//            loop += c;
+//        } else if (c == '>') p = (p == 30000) ? 30000 : p + 1;
+//        else if (c == '<') p = (p == 00000) ? 00000 : p - 1;
+//        else if (c == '+') memory[p]++;
+//        else if (c == '-') memory[p]--;
+//        else if (c == '.') putchar(memory[p]);
+//        else if (c == ',') {
+//            std::cin >> ch;
+//            memory[p] = int(ch);
+//        }
+//        else if (c == '[') {
+//            n++;
+//            if (memory[p] != 0) inLoop = 1;
+//            else inLoop = 1;
+//        } else if (c == ']') {
+//            if (--n == 0) {
+//                inLoop = 0;
+//                computeLoop(loop);
+//            }
+//        }
+//    }
+    int i = 0;
+    while (i < code.length()) {
+        char c = code[i];
+        if (c == '+') memory[p]++;
+        if (c == '-') memory[p]--;
+        if (c == '>') p++;
+        if (c == '<') p--;
+        if (c == '.') std::cout << (char)memory[p];
+        if (c == ',') {
+            u_char s;
+            std::cin >> s;
+            memory[p] = s;
         }
-        else if (c == ']') { inLoop = 0; computeLoop(loop); }
+        if (c == '[') {
+            int n = 1, j = i;
+            while (n != 0 && j < code.length()) {
+                j++;
+                if (code[j] == '[')
+                    n++;
+                if (code[j] == ']')
+                    n--;
+            }
+            std::string loop = code.substr(i+1, j - i - 1);
+            i = j;
+            computeLoop(loop);
+        }
+        i++;
     }
 }
